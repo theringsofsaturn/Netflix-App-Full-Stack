a2dd446ce55cd55edb622d159dd94df44c399ab3
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-09 01:27:46 +0100

    routes for app.js finished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,37 @@ import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { useContext } from "react";
 const App = () => {
+  const user = true;
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+          {/* if there is user, we can go to te home page. If there is no user, go to register page */}
+          {user ? <Home /> : <Redirect to="/register" />}
         </Route>
-        <Route path="/movies">
-          <Home type="movies" />
+        <Route path="/register">
+          {/* if we don't have a user, go to register page, if we have a user, go to home page */}
+          {!user ? <Register /> : <Redirect to="/" />}
         </Route>
-        <Route path="/series">
-          <Home type="series" />
-        </Route>
-        <Route path="/watch">
-          <Watch />
-        </Route>
+        {/* if we don't have a user, go to login page, if we have a user, go to home page */}
+        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+        {/* if there is a user, you can see these pages */}
+        {user && (
+          <>
+            <Route path="/movies">
+              <Home type="movie" />
+            </Route>
+            <Route path="/series">
+              <Home type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        )}
       </Switch>
     </Router>
   );
