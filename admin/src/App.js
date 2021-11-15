b20c68d88880b9e2ee68b56fb407c933f3f179c8
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-15 03:23:03 +0100

    changes in styles
    Unfinished styles.

--- admin/src/App.js
+++ admin/src/App.js
@@ -11,6 +11,16 @@ import {
 
 import UserList from "./pages/userList/userList";
 import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+// import Login from "./pages/login/Login";
+// import { AuthContext } from "./context/authContext/AuthContext";
+// import { useContext } from "react";
+// import ListList from "./pages/listList/ListList";
+// import List from "./pages/list/List";
+// import NewList from "./pages/newList/NewList";
+// import MovieList from "./pages/movieList/MovieList";
+// import { Movie } from "@material-ui/icons";
+// import NewMovie from "./pages/newMovie/NewMovie";
 
 function App() {
   // const { user } = useContext(AuthContext);
@@ -32,6 +42,27 @@ function App() {
             <Route path="/user/:userId">
               <User />
             </Route>
+            <Route path="/newUser">
+              <NewUser />
+            </Route>
+            {/* <Route path="/movies">
+                <MovieList />
+              </Route>
+              <Route path="/movie/:movieId">
+                <Movie />
+              </Route>
+              <Route path="/newMovie">
+                <NewMovie />
+              </Route>
+              <Route path="/lists">
+                <ListList />
+              </Route>
+              <Route path="/list/:listId">
+                <List />
+              </Route>
+              <Route path="/newlist">
+                <NewList />
+              </Route> */}
           </div>
         </>
       </Switch>
