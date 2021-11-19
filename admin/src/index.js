1f97c0b47dfb4988c9ed54b4167061d290217899
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-20 00:46:20 +0100

    index.js updated with context providers

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { AuthContextProvider } from "./context/authContext/AuthContext";
+import { MovieContextProvider } from "./context/movieContext/MovieContext";
+import { ListContextProvider } from "./context/listContext/ListContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <MovieContextProvider>
+        <App />
+      </MovieContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
