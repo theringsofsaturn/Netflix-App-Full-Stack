c0b31033c7dd853db2c597fece1cfbe1ea4fb196
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-19 23:57:22 +0100

    api calls for login and auth context

--- admin/src/context/authContext/AuthContext.js
+++ admin/src/context/authContext/AuthContext.js
@@ -2,7 +2,7 @@ import AuthReducer from "./AuthReducer";
 import { createContext, useEffect, useReducer } from "react";
 
 const INITIAL_STATE = {
-  user: null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
   isFetching: false,
   error: false,
 };
@@ -10,10 +10,11 @@ const INITIAL_STATE = {
 export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({ children }) => {
-  // dispatch is the function that we will use to dispatch actions (Start Login, succes, failure etc)
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
-  // children is the component that is wrapped in the context provider. This is the component that will use the context. In this case, the component that is using the context is our application (App.js)
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
 
   return (
     <AuthContext.Provider
