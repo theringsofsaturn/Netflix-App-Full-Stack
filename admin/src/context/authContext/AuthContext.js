c5067c50c93095fc701c7b9d37f7b1406ca658fb
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-19 23:41:45 +0100

    Login

--- admin/src/context/authContext/AuthContext.js
+++ admin/src/context/authContext/AuthContext.js
@@ -12,7 +12,19 @@ export const AuthContext = createContext(INITIAL_STATE);
 export const AuthContextProvider = ({ children }) => {
   // dispatch is the function that we will use to dispatch actions (Start Login, succes, failure etc)
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
-};
 
-// children is the component that is wrapped in the context provider. This is the component that will use the context. In this case, the component that is using the context is our application (App.js)
-return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  // children is the component that is wrapped in the context provider. This is the component that will use the context. In this case, the component that is using the context is our application (App.js)
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
